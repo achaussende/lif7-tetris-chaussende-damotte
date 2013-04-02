6beff03b62ed7768fb5faa3e8199791cf5c68298
Alan.damotte@gmail.com <AlanDamotte@users.noreply.github.com>
2013-04-02 08:35:18 +0000

    update board.h, tetris.c/tetris.h

--- src/board.h
+++ src/board.h
@@ -3,25 +3,35 @@
 
 #include "piece.h"
 
+
+
+/*
+* FREE : indique que le point du tableau est vide
+* FILLED: indique que le point du tableau contient un bloc (1 ou 2)
+*/
+typedef enum  { FREE,  FILLED }Fill;
+
+
 /* Taille de la grille */
 const int GRIDGE_X = 10;
 const int GRIDGE_Y = 20;
 
+
 typedef struct
 {
-    Piece currentPiece; // Pièce actuellement sur la grille
+    Piece * currentPiece; // Pièce actuellement sur la grille
     int gridge[10][20]; //grille de taille GRIDGE_X par GRIDGE_Y ----- Erreur : variably modified 'gridge' at file scope
 }Board;
 
 /* Mutateurs & Accesseurs */
 
-void setCurrentPiece (Board board, const Piece piece);
-Piece getCurrentPiece (const Board board);
+void setCurrentPiece (Board * board, const Piece * piece);
+Piece * getCurrentPiece (const Board board);
 
 /* Autres méthodes */
 
-void initBoard (Board board);
-void freeBoard (Board board);
+void initBoard (Board * board);
+void freeBoard (Board * board);
 
 //Fonction d'initialisation de gridge ?
 
