5843499a7cbc63d5dabaad65b347055dbc6a39ce
chaussende.adrien <chaussende.adrien@977697e5-19c0-944e-81ed-b9da9fa1afd7>
2013-05-06 17:39:43 +0000

    Hard drop fonctionnel

--- trunk/src/bool.h
+++ trunk/src/bool.h
@@ -1,6 +1,6 @@
 #ifndef BOOL_H
 #define BOOL_H
 
-typedef enum {FALSE = 0, TRUE = 1} Bool; // Définition d'un type booléen : Vrai = 1, False = 0
+typedef enum {FALSE = 0, TRUE = 1} Bool; // Définition d'un type booléen : Vrai = 1, Faux = 0
 
 #endif
