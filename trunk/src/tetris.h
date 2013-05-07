fdbc2e0f8d137a1a73cec3197a0e617f70071911
chaussende.adrien <chaussende.adrien@977697e5-19c0-944e-81ed-b9da9fa1afd7>
2013-05-07 09:50:32 +0000

    Update : rajout des fonctions de test et phase de jeu pour pièce tombée

--- trunk/src/tetris.h
+++ trunk/src/tetris.h
@@ -44,10 +44,9 @@ Tree * getTetrisTreeScores(const Tetris * tetris);
 
 /* ============ Fonctions de test ============= */
 
-//Bool testFallPiece(const Piece * piece);
+Bool testFallPiece(Board * board);
 Bool testRotationPiece(Board * board, const int orientation);
 Bool testLineFilled(Board * board, const unsigned int posY);
-//Bool testCollision(const Piece * piece);
 Bool testLineEmpty(Board * board, const unsigned int posY);
 Bool isCurrentPieceMovable(Board * board, const int x, const int y);
 
@@ -57,6 +56,7 @@ Bool isCurrentPieceMovable(Board * board, const int x, const int y);
 void initTetris(Tetris * tetris, Board * board, Piece * piece, Tree * tree);
 void freeTetris(Tetris * tetris);
 Tetris * createTetris(Board * board, Piece * piece, Tree * tree);
+void gameStep(Tetris * tetris);
 
 void moveCurrentPieceDown(Board * board);
 void moveCurrentPieceLeft(Board * board);
