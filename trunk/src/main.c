ba40680b81100d16c52ff25835868f5eeada172d
chaussende.adrien <chaussende.adrien@977697e5-19c0-944e-81ed-b9da9fa1afd7>
2013-05-12 00:51:13 +0000

    Update : fonctions de test et phase de jeu pour pièce tombée quasi-fonctionnelles (la nextpiece suivante est la même que la précédent currentpiece, à chaque fois)

--- trunk/src/main.c
+++ trunk/src/main.c
@@ -10,12 +10,12 @@
 int main()
 {
     SDL sdl;
-    sdltestRegression(&sdl);
+    //sdltestRegression(&sdl);
     /*sdljeuInit(&sdl);
     sdljeuBoucle(&sdl);
     sdljeuLibere(&sdl);*/
 
-    //tetrisTestRegression();
+    tetrisTestRegression();
 
     return 0;
 }
