485e1de6d491a23857051e908f2ba6c7de316aec
chaussende.adrien <chaussende.adrien@977697e5-19c0-944e-81ed-b9da9fa1afd7>
2013-05-29 14:57:05 +0000

    Add : nouveau module node & fichier de scores Update : modif due au add

--- trunk/src/binarytrees.h
+++ trunk/src/binarytrees.h
@@ -8,90 +8,9 @@
 #ifndef BINARYTREES_H
 #define BINARYTREES_H
 
-#include "player.h"
+#include "node.h"
 
-/* ============= Structure Node ============== */
 
-/**
-    @struct Node
-    @brief Structure d'un Noeud
-*/
-typedef struct Node
-{
-    Player * value;
-    struct Node * left_child;
-    struct Node * right_child;
-}Node;
-
-/* ====== Node : Mutateurs & Accesseurs ======= */
-
-/**
-
-        @brief mutateur de valeur d'un noeud
-        @param un Node et un Player
-*/
-void setNodeValue (Node * node, const Player * player);
-
-/**
-
-        @brief assesseur de la valeur d'un noeud
-        @param un Node
-        @return un Player
-*/
-Player * getNodeValue(const Node * node);
-
-/**
-
-        @brief mutateur de valeur du fils gauche d'un noeud
-        @param un Node et un pointeur sur Noeud
-*/
-void setNodeLeft_Child(Node * node, const Node * pnode);
-
-/**
-
-        @brief assesseur de la valeur du fils gauche d'un noeud
-        @param un Node
-        @return un Node
-*/
-Node * getNodeLeft_Child(const Node * node);
-
-/**
-
-        @brief mutateur de valeur du fils droit d'un noeud
-        @param un Node et un pointeur sur Noeud
-*/
-void setNodeRight_Child(Node * node, const Node * pnode);
-
-/**
-
-        @brief assesseur de la valeur du fils droit d'un noeud
-        @param un Node
-        @return un Node
-*/
-Node * getNodeRight_Child(const Node * node);
-
-/* ============= Node : Méthodes ============== */
-/**
-
-        @brief méthode d'initialisation d'un noeud
-        @param un Node et un Player
-*/
-void initNode(Node * node, const Player * player);
-
-/**
-
-        @brief méthode de création d'un noeud
-        @param un Player
-        @return un Node
-*/
-Node * createNode(const Player * player);
-
-/**
-
-        @brief méthode de libération d'un noeud
-        @param un Node
-*/
-void freeNode(Node * node);
 
 /* ============ Structure : Tree ============== */
 /**
---
+++ trunk/src/node.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "node.h"
+
+/* ======== Mutateurs et Accesseurs ========== */
+
+void setNodeValue (Node * node, const Player * player)
+{
+    node->value = player;
+}
+
+Player * getNodeValue(const Node * node)
+{
+    return node->value;
+}
+
+void setNodeLeft_Child(Node * node, const Node * pnode)
+{
+    node->left_child = pnode;
+}
+
+Node * getNodeLeft_Child(const Node * node)
+{
+    return node->left_child;
+}
+
+void setNodeRight_Child(Node * node, const Node * pnode)
+{
+    node->right_child = pnode;
+}
+
+Node * getNodeRight_Child(const Node * node)
+{
+    return node->right_child;
+}
+
+/* ================ Méthodes ================= */
+
+void initNode(Node * node, const Player * player)
+{
+    /* Vérification fiche joueur valide */
+
+    assert(player->score >= 0 && strlen(player->name)>0);
+
+    /* Initialisation */
+
+    setNodeValue(node, player);
+    setNodeLeft_Child(node, NULL);
+    setNodeRight_Child(node, NULL);
+}
+
+Node * createNode(const Player * player)
+{
+    Node * node = (Node *)malloc(sizeof(Node));
+
+    initNode(node, player);
+    return node;
+}
+
+void freeNode(Node * node)
+{
+    setNodeLeft_Child(node, NULL);
+    setNodeRight_Child(node, NULL);
+    free(node->value);
+    free(node);
+}
